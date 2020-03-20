1e8c70b3458867e7e69ca1a92524409c30c89ced
Marie Seindal <marie@seindal.dk>
2020-03-20 15:28:35 +0100

    Input and sorting

--- src/Mandatory3/ChristmasLight.java
+++ src/Mandatory3/ChristmasLight.java
@@ -1,4 +1,30 @@
 package Mandatory3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class ChristmasLight {
+    public static void main(String[] args) {
+
+////////// take input
+        Scanner sc = new Scanner(System.in);
+        int numOfhouses = sc.nextInt();
+        ArrayList<Integer> houses = new ArrayList<>();
+        for (int i = 0; i < numOfhouses; i++) {
+            houses.add(sc.nextInt());
+        }
+
+////////// Sort list of houses
+        Collections.sort(houses);
+        System.out.println();
+        Arrays.toString(houses.toArray());
+
+
+////////// Find the longest chain
+
+
+
+    }
 }
