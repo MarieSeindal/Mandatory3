acde4f8cbb44819bf21a6a9c1a33107e606ed68e
Marie Seindal <marie@seindal.dk>
2020-03-20 16:04:54 +0100

    Done, og virker på codejudge

--- src/Mandatory3/ChristmasLight.java
+++ src/Mandatory3/ChristmasLight.java
@@ -1,7 +1,6 @@
 package Mandatory3;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
@@ -18,12 +17,25 @@ public class ChristmasLight {
 
 ////////// Sort list of houses
         Collections.sort(houses);
-        System.out.println();
-        Arrays.toString(houses.toArray());
-
 
 ////////// Find the longest chain
+        int longestCount=0;
+        int currentCount=0;
 
+        for (int i = 0; i<numOfhouses-1; i++){
+            //System.out.println("Comparing: " + houses.get(i) + "+1 " + houses.get(i+1) );
+            if (houses.get(i)+1 == houses.get(i+1)){
+                //Hvis det nuværende husnummer+1 er lig med det næste husnummer
+                currentCount++;
+                if (currentCount>longestCount) //Find kun den største kæde af husnumre
+                    longestCount=currentCount;
+            }
+            else
+                currentCount=0;
+
+        }
+        longestCount++; //Jeg tæller alle "Forbindelserne" mellem husene, og den vil gerne have antal huse. Derfor +1.
+        System.out.println(longestCount);
 
 
     }
