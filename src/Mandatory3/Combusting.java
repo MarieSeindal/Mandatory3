febc435c9194a011b52b046c8246b13babcfa22c
Marie Seindal <marie@seindal.dk>
2020-03-21 14:23:18 +0100

    Tæt på, Den eneste fejl er at jeg bare ikke kan få den til at tilføje mere en 1 element til den ene potte

--- src/Mandatory3/Combusting.java
+++ src/Mandatory3/Combusting.java
@@ -29,17 +29,7 @@ public class Combusting {
 
         // Array fra 0 til det højeste nr. indgredient
         int[] indgredients = new int[numOfIngredients];
-        int[] naboListe = new int[numOfIngredients-1];
-//        for (int i =0; i<numOfIngredients; i++){
-//            indgredients[i]=i;                              // Dette er alle ingredienser, som skal tjekkes om de siger boom.
-//                                                            // Dette er alle ingredienser, som skal ind på nabolisten. Senere kommer deres naboer ind på
-//
-//        }
-
-//        LinkedList<Integer> boomListe = new LinkedList<>();
-//        for (int i =0; i<numOfBoomPairs*2; i++){
-//            naboListe.
-//        }
+        int[] naboListe = new int[numOfIngredients - 1];
 
         // generer 2d array med boom-par
         int[][] boomInput = new int[2][numOfBoomPairs];
@@ -52,16 +42,26 @@ public class Combusting {
         boolean[][] boomTable = new boolean[numOfIngredients][numOfIngredients];
         for (int i = 0; i < numOfIngredients; i++) {
             for (int j = 0; j < numOfIngredients; j++) {
-                boomTable[i][j]=false;
+                boomTable[i][j] = false;
             }
         }
         for (int i = 0; i < numOfBoomPairs; i++) {
-            boomTable[ boomInput[0][i] ][ boomInput[1][i] ]=true;
+            boomTable[boomInput[0][i]] [boomInput[1][i]] = true;
+            boomTable[boomInput[1][i]] [boomInput[0][i]] = true;
         }
 
         for (int i = 0; i < numOfIngredients; i++) { //todo
+            for (int j = 0; j < numOfIngredients; j++) {
+                System.out.print(boomTable[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+
+
+        for (int i = 0; i < numOfIngredients; i++) { //todo
             for (int j = 0; j < numOfIngredients; j++) { //todo
-                System.out.println("BoomTable efter boom markering: " + boomTable[i][j] + ", index: " + i + ";" + j); //todo
+                //System.out.println("BoomTable efter boom markering: " + boomTable[i][j] + ", index: " + i + ";" + j); //todo
             } //todo
         } // todo
 
@@ -71,53 +71,68 @@ public class Combusting {
         boolean[] potte1 = new boolean[numOfIngredients]; // initialiserer vores potter
         boolean[] potte2 = new boolean[numOfIngredients];
 
-//        for (int i = 0; i < numOfIngredients; i++) { //todo Irrelevant, da den er flase før jeg sætter den til false
-//            System.out.println("Potte tjek1, potte 1: " + potte1[i] + ", potte 2: " + potte2[i] + ", index: " + i);
-//            potte1[i] = false;
-//            potte2[i] = false;
-//            System.out.println("Potte tjek2, potte 1: " + potte1[i] + ", potte 2: " + potte2[i] + ", index: " + i);
-//
-//        }
+        for (int i = 0; i < numOfIngredients; i++) { //todo Irrelevant, da den er flase før jeg sætter den til false???
+            //System.out.println("Potte tjek1, potte 1: " + potte1[i] + ", potte 2: " + potte2[i] + ", index: " + i);
+            potte1[i] = false;
+            potte2[i] = false;
+            //System.out.println("Potte tjek2, potte 1: " + potte1[i] + ", potte 2: " + potte2[i] + ", index: " + i);
+
+        }
 
         boolean boom = false; // Vi er ikke eksploderet endnu ;-)
 
-        for (int i = 0; i < numOfIngredients; i++) {
+        for (int i = 0; i < numOfIngredients; i++) { //Tjek hver ingrediens
 
             //Tjek potte 1
             //Sammenlig indgredient i med boomTable. Den er true hvor der er ulovlige forbindelser
-            for (int j = 0; j < numOfIngredients; j++) {
-                if (!boomTable[i][j] && !boomTable[j][i]) { //Hvis den er der, skal den ud af for-loopet og fortsætte til potte 2
-                    if(j==numOfIngredients-1) {
-                        potte1[i] = true; // Hvis den ikke finder en ulovlig nabo, så "Put den i potten"
-                        System.out.println("Potte 1 tilføjet: " + i + ";" + j); //todo
+            for (int j = 0; j < numOfIngredients; j++) { // tjek for hver potentiel nabo (aka hver ingrediens)
+
+                boolean notPotte1 = false; // if true, then its not in potte 1
+                for (j = j; j < numOfIngredients; j++) { // potte 1
+
+                    if (boomTable[i][j] || boomTable[j][i]) { //Hvis den er der, skal den ud af for-loopet og fortsætte til potte 2
+                        System.out.println(i + " har ugyldig nabo: " + j);
+                        notPotte1 = true;
+                        j++;
+                        break;
                     }
-
-                } else {
-                    break;
-                }
-            }
-
-
-            //Tjek potte 2
-            //Sammenlig indgredient i med boomTable. Den er true hvor der er ulovlige forbindelser
-            if (!potte1[i]) { // Hvis den ikke er i potte 1, så tjek her her
-                for (int j = 0; j < numOfIngredients; j++) {
-                    if (!boomTable[i][j] && !boomTable[j][i]) { //Hvis den også er her, skal den ud af for-loopet og BOOM! Da der ikke er nogen mulig plads til den.
-                        if (j==numOfIngredients) {
-                            potte2[i] = true; // Hvis den ikke finder en ulovlig nabo, så "Put den i potten"
-                            System.out.println("Potte 2 tilføjet: " + i + ";" + j); //todo
-                        }
-                    } else {
-                        boom = true; // Sætter boom til true
-                        System.out.println("Oh no " + i + ";" + j); //todo
-                        System.out.println("BOOM!");
+                    // Da vi lægger dem i potten i kronologisk rækkefølge, så behøver vi ikke tjekke efer den nabo vi er nået til, (Ny linje)
+                    // da vi ikke har putttet efterfølge naboer i endnu.
+                    else if (j == i) {
+                        potte1[i] = true; // Hvis den ikke finder en ulovlig nabo, så "Put den i potten"
+                        System.out.println("Potte 1 tilføjet indegrediens: " + i); //todo
+                        break;
+                    }
+                    else if(!notPotte1){
+                        potte1[i] = true; // Hvis den ikke finder en ulovlig nabo, så "Put den i potten"
+                        System.out.println("Potte 1 tilføjet indegrediens: " + i); //todo
                         break;
                     }
 
                 }
+
+                if (!potte1[i]) { // Hvis den ikke er i potte 1, så tjek her her
+                    for (j = j; j < numOfIngredients; j++) {
+                        if (boomTable[i][j] || boomTable[j][i]) { //Hvis den også er her, skal den ud af for-loopet og BOOM! Da der ikke er nogen mulig plads til den.
+
+                            System.out.println(i + " har ugyldig nabo: " + j);
+                            boom = true; // Sætter boom til true
+                            System.out.println("BOOM!");
+                            break;
+
+                        }
+                        if (!boom) {
+                            potte2[i] = true; // Hvis den ikke finder en ulovlig nabo, så "Put den i potten"
+                            System.out.println("Potte 2 tilføjet indgrediens: " + i); //todo
+                            break;
+                        }
+                    }
+
+                }
+                break;
             }
             //Tjek om boom er opstået, hvis ja bryd ud
-            if(boom)
+            if (boom)
                 break;
 
         }
@@ -125,18 +140,18 @@ public class Combusting {
 ////////Print resultat
 
         //Generer resultat potte 1
-        String potte1Res ="";
+        String potte1Res = "";
         for (int i = 0; i < numOfIngredients; i++) { //Aflæs potte 1
-            if(potte1[i]) {
+            if (potte1[i]) {
                 potte1Res += i;
                 potte1Res += " ";
             }
         }
 
         //Generer resultat potte 2
-        String potte2Res ="";
+        String potte2Res = "";
         for (int i = 0; i < numOfIngredients; i++) { //Aflæs potte 2
-            if(potte2[i]) {
+            if (potte2[i]) {
                 potte2Res += i;
                 potte2Res += " ";
             }
@@ -146,6 +161,7 @@ public class Combusting {
             System.out.println(potte1Res + "\n" + potte2Res);
         else if (!boom && potte1Res.length() <= potte2Res.length())
             System.out.println(potte2Res + "\n" + potte1Res);
+
 
 
     }
