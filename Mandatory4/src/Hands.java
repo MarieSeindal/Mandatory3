4199b23794174b01668876105bf49155342c0ad0
Marie Seindal <marie@seindal.dk>
2020-04-02 17:03:05 +0200

    jeg kan ikke få det til at virke endnu

--- Mandatory4/src/Hands.java
+++ Mandatory4/src/Hands.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Hands {
 
@@ -18,40 +19,55 @@ public class Hands {
         int peopleNowInfected = 1;
         int amountOfHandshakes = sc.nextInt();
 
+////////// https://www.baeldung.com/java-multi-dimensional-arraylist
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>(totalPeople);
+
+        for (int i = 0; i < totalPeople; i++) {
+            graph.add(new ArrayList());
+        }
         
         for (int i = 0; i < amountOfHandshakes; i++) { // graph with handshakes
             int a = sc.nextInt();
             int b = sc.nextInt();
             graph.get(a).add(b);
-            graph.get(b).add(a);
+            //graph.get(b).add(a);
+            //todo test for the array array thing is alright
+            System.out.println(graph.toString());
         }
+////////////////////////////////////////////////////////////////////////
 
-        
+
+
         boolean[] infected = new boolean[totalPeople];
-        infected[patientZero]=true; // true infect pation 0
+        infected[patientZero]=true; // true infect patient 0
 
-        // make a boolean table totPeop x totPeop, where its set to true if the index people know each other.
+        //Current person to check handshakes.
+        int currentP = patientZero;
+        Stack<Integer> neigboursToBeChecked = new Stack<Integer>();
+        neigboursToBeChecked.push(patientZero);
 
-        boolean[][] handshakeTable = new boolean[totalPeople][totalPeople];
-
-        for (int i = 0; i <totalPeople; i++) {
-            int currentP = patientZero;
-
-            for (int j = 0; j < graph.get(patientZero).size(); j++) { //
+        boolean end = false;
+        while(!end){
+            
+            for (int j = 0; j < graph.get(currentP).size(); j++) { // infects all the neighbors of node currentP
+                infected[graph.get(currentP).get(j)]=true;
+                neigboursToBeChecked.push(graph.get(currentP).get(j)); // adds the neigbour to a stack of neighbours to check through
+                peopleNowInfected++;
 
             }
 
+            currentP = neigboursToBeChecked.peek(); // selects the nex neighbour to infect people
+            neigboursToBeChecked.pop();
+            System.out.println(currentP); // todo
+
+
+            if (neigboursToBeChecked.empty()) {
+                end = true;
+                System.out.println(peopleNowInfected);
+            }
 
         }
 
-        
-//////////Logic
-
-
-
-
-
-
     }
+
 }
